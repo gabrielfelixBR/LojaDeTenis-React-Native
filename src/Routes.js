14a5d2fcd8b84a9a62764000bbbbc66e8ae8c41f
Felix <mrgabrielfelixsilva@gmail.com>
2025-03-29 19:10:02 -0300

    :tada: Projeto 100% Pronto

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
+import { Feather, Ionicons } from '@expo/vector-icons'
+import { TouchableOpacity } from "react-native";
 
 import Home from './pages/Home'
 import Detail from './pages/Detail'
@@ -19,6 +21,34 @@ function Routes(){
                 <Stack.Screen
                     name="Detail"
                     component={Detail}
+                    options={({ navigation }) => ({
+                        headerTitleAlign: 'left',
+                        headerTitleStyle: { 
+                            fontSize: 20, 
+                            fontWeight: 'bold', 
+                        },
+                        headerLeft: () => (
+                            <TouchableOpacity 
+                              onPress={() => navigation.goBack()} 
+                              style={{ marginLeft: 10 }}
+                            >
+                                <Ionicons 
+                                    name="chevron-back" 
+                                    size={32} 
+                                    color="black" 
+                                />
+                            </TouchableOpacity>
+                        ),
+                        headerRight: () => (
+                            <TouchableOpacity style={{marginRight: 15}}>
+                                <Feather 
+                                    name="shopping-cart"
+                                    size={24}
+                                    color="black"
+                                />
+                            </TouchableOpacity>
+                        )
+                    })}
                 />
             </Stack.Navigator>
         </NavigationContainer>
