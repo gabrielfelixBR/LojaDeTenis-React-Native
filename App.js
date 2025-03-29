a18dcf92d8cf397b10720a6096ac47ca6cd9aa69
Felix <mrgabrielfelixsilva@gmail.com>
2025-03-29 13:53:43 -0300

    :white_check_mark: Dependencias baixadas

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Inicio do Projeto!</Text>
+      <Text>Inicio do Projeto</Text>
     </View>
   );
 }
